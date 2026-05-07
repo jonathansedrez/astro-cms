0ad58df2e68408801e5fbb833960651b1b0948d8
Jonathan Sedrez <jonathansedrez@gmail.com>
2026-05-07 20:57:09 -0300

    feat: terraform init

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 ## Configure localstack
 
 `docker run -d -p 4566:4566 --name localstack localstack/localstack:3.8.0` -> Setting a version to garantee it is a free tier.
+
+## Terraform install
+
+```
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
+```
+
+## Terraform init
+```
+terraform init
+```
---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
+provider "aws" { 
+  region = "us-east-1"
+  access_key = "test" # LocalStack only
+  secret_key = "test" # LocalStack only
+  skip_credentials_validation = true # LocalStack only
+  skip_metadata_api_check     = true # LocalStack only
+  skip_requesting_account_id  = true # LocalStack only
+
+  endpoints {
+    s3 = "http://localhost:4566" # LocalStack only
+  }
+}
+
+resource "aws_s3_bucket" "main" {
+  bucket = "my-first-s3-bucket"
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.main.arn # Refers to line 25
+}
