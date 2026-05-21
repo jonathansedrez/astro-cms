3b246535481fdb0a3081e9458692745e930891c8
Jonathan Sedrez <jonathansedrez@gmail.com>
2026-05-20 21:34:57 -0300

    feat: consume content from s3

--- content.config.ts
+++ content.config.ts
@@ -1,9 +1,14 @@
 import { defineCollection } from "astro:content";
 import { z } from "astro/zod";
-import { glob } from "astro/loaders";
+import { s3Loader } from "astro-loader-s3";
 
 const documents = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/content/documentation" }),
+  loader: s3Loader({
+    bucket: import.meta.env.S3_BUCKET,
+    accessKeyId: import.meta.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: import.meta.env.AWS_SECRET_ACCESS_KEY,
+    region: import.meta.env.AWS_REGION,
+  }),
   schema: z.object({
     title: z.string(),
   }),
